dd425fa02ebd47fca4814c7a57cf3542ba275b6b
Anas <anasabughazaal@gmail.com>
2022-11-02 05:19:42 +0300

    first commit

---
+++ js/scroll_Header.js
@@ -0,0 +1,10 @@
+$(function(){
+      var navbar = $('.header-inner');
+      $(window).scroll(function(){
+        if($(window).scrollTop() <=40){
+          navbar.removeClass('navbar-scroll');
+        }else{
+          navbar.addClass('navbar-scroll');
+        }
+      });
+    });
